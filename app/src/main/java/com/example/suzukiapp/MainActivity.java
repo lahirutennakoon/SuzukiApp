c69e071e3b174806ccde70d6c90f4f1e645297fb
Lahiru Tennakoon <lahiru.tennakoon@my.sliit.lk>
2018-03-25 20:09:09 +0530

    created app bar and context menu

--- app/src/main/java/com/example/suzukiapp/MainActivity.java
+++ app/src/main/java/com/example/suzukiapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.suzukiapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set the toolbar as the app bar for the activity
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
     }
+
+    //create context menu (i.e. "Settings" menu)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        return super.onOptionsItemSelected(item);
+    }
+
+    //go to login page
+    public void goToLoginPage(View view)
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
 }
