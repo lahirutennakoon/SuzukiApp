33cfddefba577eeb0c5cc45b4bc9f93a68303e59
Lahiru Tennakoon <lahiru.tennakoon@my.sliit.lk>
2018-03-31 11:27:36 +0530

    view car details

--- app/src/main/java/com/example/suzukiapp/CarDetailsActivity.java
+++ app/src/main/java/com/example/suzukiapp/CarDetailsActivity.java
@@ -6,13 +6,26 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
+import com.example.suzukiapp.model.Car;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
 
 public class CarDetailsActivity extends AppCompatActivity
 {
     //reference to firebase database (for textual data)
     private DatabaseReference databaseReference;
+
+    //reference to firebase storage for images
+    private FirebaseStorage firebaseStorage;
+
+    private StorageReference storageReference;
 
     private TextView textView;
 
@@ -39,9 +52,93 @@ public class CarDetailsActivity extends AppCompatActivity
         databaseReference = FirebaseDatabase.getInstance().getReference().child("Car");
 
         //get previous activity's clicked item to text view
-        String model = getIntent().getExtras().getString("model");
-        System.out.println("Model: " + model);
-        textView = (TextView) findViewById(R.id.carDetails);
-        textView.setText(model);
+        final String modelFromMainActivity = getIntent().getExtras().getString("model");
+        System.out.println("Model: " + modelFromMainActivity);
+        textView = (TextView) findViewById(R.id.model);
+        textView.setText("Model: " + modelFromMainActivity);
+
+        databaseReference.addChildEventListener(new ChildEventListener()
+        {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s)
+            {
+                Car car = (Car) dataSnapshot.getValue(Car.class);
+
+                String modelFromDatabse = null;
+                if (car != null)
+                {
+                    modelFromDatabse = car.getModel();
+                }
+
+                if (modelFromMainActivity != null && modelFromMainActivity.equals(modelFromDatabse))
+                {
+                    int doorCount = car.getDoorCount();
+                    textView = (TextView) findViewById(R.id.doorCount);
+                    textView.setText("Door Count: " + doorCount);
+
+                    String colorsAvailable = car.getColorsAvailable();
+                    textView = (TextView) findViewById(R.id.colorsAvailable);
+                    textView.setText("Colors Available: " + colorsAvailable);
+
+                    String fuelType = car.getFuelType();
+                    textView = (TextView) findViewById(R.id.fuelType);
+                    textView.setText("Fuel Type: " + fuelType);
+
+                    double engineCapacity = car.getEngineCapacity();
+                    textView = (TextView) findViewById(R.id.engineCapacity);
+                    textView.setText("Engine Capacity (cc) : " + engineCapacity);
+
+                    String transmission = car.getTransmission();
+                    textView = (TextView) findViewById(R.id.transmission);
+                    textView.setText("Transmission: " + transmission);
+
+                    String releaseYear = car.getReleaseYear();
+                    textView = (TextView) findViewById(R.id.releaseYear);
+                    textView.setText("Release Year: " + releaseYear);
+
+                    double price = car.getPrice();
+                    textView = (TextView) findViewById(R.id.price);
+                    textView.setText("Price(Rs.): " + price);
+
+                    String features = car.getFeatures();
+                    textView = (TextView) findViewById(R.id.features);
+                    textView.setText("Features: " + features);
+
+
+                    System.out.println("doorCount:" + doorCount);
+                    System.out.println("colorsAvailable:" + colorsAvailable);
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s)
+            {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot)
+            {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s)
+            {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError)
+            {
+
+            }
+        });
+
+        //download car image
+        storageReference = firebaseStorage.getReference().child("Car/" + modelFromMainActivity + ".jpg");
+
+        File file = File.createTempFile()
     }
 }
